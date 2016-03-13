ad2ef89a2765778b8bae76b0ebe96d64f61ef984
Propuhganduh <JosephLG27@outlook.com>
2016-03-13 17:59:03 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,7 +62,8 @@ function respond() {
   } 
   else if(request.text && botRegexSb.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://i.imgur.com/yNBtNiy.gif");
+    postMessage("http://i.imgur.com/yNBtNiy.gif
+    GG Guys");
     this.res.end();
   } 
   else if(request.text && botRegexLab.test(request.text)) {
